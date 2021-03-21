7df14139c4c033cb88adb8188ec6dde09199685a
Tiqur <Tiqur7@gmail.com>
2021-03-21 03:27:08 -0700

    Setup navbar component

---
+++ ichis/src/components/NavBar.jsx
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+import FlexDiv from './FlexDiv';
+
+
+const NavContainer = styled(FlexDiv)`
+    flex-direction: row;
+    background: #121212;
+`;
+
+const NavBar = (props) => {
+    return (
+        <NavContainer>
+
+        </NavContainer>
+    )
+};
+
+export default NavBar;
