dee946e87f4ce69f67e1a2ef9cb2d8ae386730d6
Tiqur <Tiqur7@gmail.com>
2021-03-21 04:49:45 -0700

    Navbar scroll shadow

--- ichis/src/components/NavBar.jsx
+++ ichis/src/components/NavBar.jsx
@@ -2,19 +2,32 @@ import styled from 'styled-components';
 import FlexDiv from './FlexDiv';
 import GithubIcon from '../assets/github-icon.png'
 import HamburgerIcon from './HamburgerIcon';
+import { useEffect, useState } from 'react';
 
 
 const NavContainer = styled(FlexDiv)`
     flex-direction: row;
     align-items: center;
-    background: red;
+    background: #121212;
+    transition: 200ms;
+    position: fixed;
+    width: 100%;
     height: 50px;
-    height: 50px;
+    filter: ${props => props.scrolled ? 'drop-shadow(0px 0px 4px #000000)' : ''};
 `;
 
 const NavBar = (props) => {
+    
+    const [isScrolled, setScrolled] = useState(false);
+
+    useEffect(() => {
+        document.addEventListener('scroll', () => {
+            setScrolled(window.pageYOffset > 0);
+        })
+    }, []);
+
     return (
-        <NavContainer>
+        <NavContainer scrolled={isScrolled}>
             <HamburgerIcon />
             <img src={GithubIcon} style={{position: "absolute", right: "8px", height: "40px"}}/>
         </NavContainer>
--- ichis/src/pages/HomePage.jsx
+++ ichis/src/pages/HomePage.jsx
@@ -1,8 +1,19 @@
+import styled from 'styled-components';
+import FlexDiv from '../components/FlexDiv';
 import NavBar from '../components/NavBar';
+
+
+const Container = styled(FlexDiv)`
+    flex-direction: column;
+`;
+
+
 
 const HomePage = (props) => {
     return (
-        <NavBar />
+        <Container>
+            <NavBar />
+        </Container>
     )
 };
 
