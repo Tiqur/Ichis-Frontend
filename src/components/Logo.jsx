c7cd6fae0d24540285834599a3087d49a6f98e2b
Tiqur <Tiqur7@gmail.com>
2021-03-26 03:08:42 -0700

    Logo animation

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef } from 'react';
 import styled from 'styled-components';
 import FlexDiv from './FlexDiv';
 
@@ -5,8 +6,9 @@ import FlexDiv from './FlexDiv';
 const Container = styled(FlexDiv)`
     justify-content: center;
     align-items: center;
+    margin-bottom: -50px;
     height: 100px;
-    width: 200px;
+    width: auto;
     transition: 500ms;
 `;
 
@@ -17,26 +19,48 @@ const Square = styled.div`
     user-select: none;
     margin-bottom: 30px;
     transition: 300ms;
-    transform: translateX(${props => props.height+54}px) translateY(${props => props.height}px) rotateZ(45deg);
+    transform: translateX(${props => props.height+24}px) translateY(${props => props.height}px) rotateZ(45deg);
 `;
 
 const Triangle = styled.div`
-    border-left: ${props => props.size / 2}px solid transparent;
-    border-right: ${props => props.size / 2}px solid transparent;
-    border-bottom: ${props => props.size / 2}px solid ${props => props.color};
+    border-left: ${props => props.size / 4}px solid transparent;
+    border-right: ${props => props.size / 4}px solid transparent;
+    border-bottom: ${props => props.size / 4}px solid ${props => props.color};
     transform: translateY(${props => props.height}px) translateX(${props => props.tx}px);
     user-select: none;
     margin-right: 10px;
 `;
 
+const AnimateSquares = (ref, h) => {
+    const height = 50;
+    ref.style.transform = `translateX(${24}px) translateY(${-4-height}px) rotateZ(0deg)`;
+    setTimeout(() => {
+        ref.style.transform = `translateX(${h+24}px) translateY(${h}px) rotateZ(-45deg)`;
+    }, 301);
+};
+
 const Logo = (props) => {
+    const sr1 = useRef(null);
+    const sr2 = useRef(null);
+    const sr3 = useRef(null);
+    const s1h = -4;
+    const s2h = 4;
+    const s3h = 12;
+
+    useEffect(() => {
+        // Animate logo on page load
+        setTimeout(() => {AnimateSquares(sr1.current, s1h)}, 1100);
+        setTimeout(() => {AnimateSquares(sr2.current, s2h)}, 1200);
+        setTimeout(() => {AnimateSquares(sr3.current, s3h)}, 1300);
+    }, []);
+
     return (
         <Container>
-            <Square height={-4} size={props.size / 2} color='#6C63FF' id='s1'/>
-            <Square height={4}  size={props.size / 2} color='#6C63FF' id='s2'/>
-            <Square height={12} size={props.size / 2} color='#6C63FF' id='s3'/>
-            <Triangle height={-2} tx={-2} size={props.size / 2} color='#948DFF'/>
-            <Triangle height={5}  tx={-8} size={props.size / 2} color='#948DFF'/>
+            <Square height={s1h} size={props.size} color='#6C63FF' ref={sr1}/>
+            <Square height={s2h}  size={props.size} color='#6C63FF' ref={sr2}/>
+            <Square height={s3h} size={props.size} color='#6C63FF' ref={sr3}/>
+            <Triangle height={-2} tx={-32} size={props.size} color='#948DFF'/>
+            <Triangle height={5}  tx={-38} size={props.size} color='#948DFF'/>
         </Container>
     )
 };
