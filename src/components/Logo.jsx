27e28faa6eae482e535de07b231471dfd9cb40e7
Tiqur <Tiqur7@gmail.com>
2021-03-25 19:37:46 -0700

    Setup logo component

---
+++ src/components/Logo.jsx
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+const Square = styled.div`
+    height: ${props => props.size};
+    width: ${props => props.size};
+    color: ${props => props.color};
+`;
+
+const Triangle = styled.div`
+    width: 0;
+    height: 0;
+    border-left: ${props.size / 2}px solid transparent;
+    border-right: ${props.size / 2}px solid transparent;
+    border-bottom: ${props.size}px solid ${props.color};
+`;
+
+const Logo = () => {
+    return (
+        <p></p>
+    )
+};
+
+export default Logo;
