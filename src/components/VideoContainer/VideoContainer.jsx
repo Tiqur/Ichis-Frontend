26bcfc7432fb17b87f328cd2b293ec7b0693a781
Tiqur <trevorbrage02@gmail.com>
2021-04-07 02:05:51 -0700

    Added ability to play / stream videos

--- src/components/VideoContainer/VideoContainer.jsx
+++ src/components/VideoContainer/VideoContainer.jsx
@@ -1,11 +1,16 @@
-import classNames from 'classnames';
+import classNames from 'classnames/bind';
 import styles from './VideoContainer.module.scss';
+import ReactPlayer from 'react-player';
+
 
 const VideoContainer = () => {
   const cn = classNames.bind(styles);
 
   return (
     <div className={cn(styles.container)}>
+      <ReactPlayer className={cn(styles.video)}
+        url='https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+      />
     </div>
   )
 }
