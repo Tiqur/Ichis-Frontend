a648cd10426d6fa276d277a513fea70d5f332114
Tiqur <trevorbrage02@gmail.com>
2021-04-07 21:38:17 -0700

    Video queue container ( width > 800px )

---
+++ src/components/VideoQueue/VideoQueue.jsx
@@ -0,0 +1,14 @@
+import classNames from 'classnames/bind';
+import styles from './VideoQueue.module.scss';
+
+const VideoQueue = () => {
+  const cn = classNames.bind(styles);
+
+  return (
+    <div className={cn(styles.container)}>
+
+    </div>
+  )
+}
+
+export default VideoQueue;
--- src/pages/DashboardPage/DashboardPage.jsx
+++ src/pages/DashboardPage/DashboardPage.jsx
@@ -4,6 +4,7 @@ import NavBar from '../../components/NavBar';
 import VideoContainer from '../../components/VideoContainer/VideoContainer';
 import Layout from '../../components/Layout';
 import ChatBox from '../../components/ChatBox/ChatBox';
+import VideoQueue from '../../components/VideoQueue/VideoQueue';
 
 const DashboardPage = () => {
   const cn = classNames.bind(styles);
@@ -11,10 +12,11 @@ const DashboardPage = () => {
   return (
     <Layout>
         <NavBar />
-      <div className={cn(styles.page_grid_layout)}>
-        <VideoContainer />
-        <ChatBox />
-      </div>
+        <div className={cn(styles.page_grid_layout)}>
+          <VideoContainer />
+          <VideoQueue />
+          <ChatBox />
+        </div>
     </Layout>
   )
 }
